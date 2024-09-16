038949c731e4f137bea841a27d01703dea234f9e
WorknBuyConsumenDie <a_chapala@mail.ru>
2024-09-16 23:55:23 +1000

    small fixes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import ThemeSwitcher from './ThemeSwitcher';
 import './Navbar.css'; // Assuming your CSS is in Navbar.css
